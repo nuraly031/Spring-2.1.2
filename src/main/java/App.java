71317f973ce287c0498b16753f5221f15c6bea5f
Nuraly Sharapiyev <r.sharapiev@gmail.com>
2024-04-22 10:56:54 +0300

    Finish

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,20 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean1.getMessage());
+
+        boolean a = bean == bean1;
+        System.out.println(a);
+
+        Cat bean11 = (Cat) applicationContext.getBean("cat");
+        System.out.println(bean11.getMessage());
+
+        Cat bean22 = (Cat) applicationContext.getBean("cat");
+        System.out.println(bean22.getMessage());
+
+        boolean b = bean11 == bean22;
+        System.out.println(b);
     }
 }
